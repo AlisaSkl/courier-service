3ddd10321049481e350874df8d50ba827b12b0b0
Alisa <alisasklrv@gmail.com>
2024-05-08 15:47:22 +0300

    create models Courier, District, Restaurant

--- courier/models.py
+++ courier/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from courier_service import settings
+
+
+class District(models.Model):
+    name = models.CharField(max_length=255)
+    courier = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        related_name="couriers"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    district = models.ForeignKey(
+        District,
+        on_delete=models.CASCADE,
+        related_name="restaurants"
+    )
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
+
+class Courier(AbstractUser):
+    driving_license = models.CharField(max_length=9),
+
+    class Meta:
+        ordering = ("username", )
